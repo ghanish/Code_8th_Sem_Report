8f5774d64e3d49d0c5467ef9195226a527a9bf5d
Ghanish Garg <33462548+ghanish@users.noreply.github.com>
2021-08-08 20:24:56 +0530

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusesBooking
+{
+    public static class Constants
+    {
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+       
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+﻿Admin Detail 
+email: admin@busesBooking.com
+Password : Admin@123
+
+Customer Detail Self Created 
+email : cust@gmail.com
+Password:Admin@123
